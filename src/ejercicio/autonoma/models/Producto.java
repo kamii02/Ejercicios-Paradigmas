4ed22748e0c0408fe9954ea974f7ea0c8d825763
kamii02 <camilaauto10@gmail.com>
2025-08-15 21:51:14 -0500

    se organizaron y crearon algunos metodos

--- src/ejercicio/autonoma/models/Producto.java
+++ src/ejercicio/autonoma/models/Producto.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejercicio.autonoma.models;
 
 /**
@@ -9,5 +5,61 @@ package ejercicio.autonoma.models;
  * @author camil
  */
 public class Producto {
+    private String nombre;
+    private String categoria;
+    private double precio;
+    private int stock;
+    private int unidadesVendidas; 
+
+    public Producto(String nombre, String categoria, double precio, int stock, int unidadesVendidas) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+        this.unidadesVendidas = unidadesVendidas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getUnidadesVendidas() {
+        return unidadesVendidas;
+    }
+
+    public void setUnidadesVendidas(int unidadesVendidas) {
+        this.unidadesVendidas = unidadesVendidas;
+    }
+    
     
 }
+
+    
--- src/ejercicio/autonoma/models/Tienda.java
+++ src/ejercicio/autonoma/models/Tienda.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejercicio.autonoma.models;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -10,4 +8,61 @@ package ejercicio.autonoma.models;
  */
 public class Tienda {
     
+    private ArrayList<Producto> productos;
+    
+
+    public Tienda() {
+        this.productos = new ArrayList<>();;
+    }
+    
+    public boolean agregarProducto(Producto p){
+        return productos.add(p);
+    } 
+    
+    public void productoElectronicaConExistenciaMayor0(){
+        for(Producto p: productos){
+            if(p.getCategoria().equalsIgnoreCase("Electronica") && p.getStock()>0){
+                
+            }
+        }
+    }
+    
+    public void aumentarPrecioCategoriaHogar(){
+        for(Producto p: productos){
+            if(p.getCategoria().equalsIgnoreCase("Electronica") && p.getStock()<5){
+                double aumentoDiezPorciento = p.getPrecio()*0.10;
+                double nuevoPrecio = p.getPrecio()+aumentoDiezPorciento;
+                p.setPrecio(nuevoPrecio);
+            }
+        }
+    }
+    
+    public double  ingresosTotalesDeCadaCategoria(String categoria){
+        double ingresos = 0;
+        for(Producto p: productos){
+            if(p.getCategoria().equalsIgnoreCase(categoria)){
+               ingresos += p.getPrecio() * p.getUnidadesVendidas();
+            }
+        }
+        return ingresos;
+    }
+    
+    public String imprimirCategoriaConMasIngresos(){
+        double ingresosMax = 0;
+        String categoriaMax = "";
+        for(Producto p: productos){
+            String categoria = p.getCategoria();
+            double ingresosCategoria = ingresosTotalesDeCadaCategoria(categoria);
+            if(ingresosCategoria > ingresosMax){
+                ingresosMax = ingresosCategoria;
+                categoriaMax = categoria;
+            }
+        }
+        return categoriaMax;
+    }
+    
+    public void nombresProductosOrdenadosPrecioDescendente(){
+        ArrayList<Producto> productosOrdenadosPrecioDescendente;
+    }
 }
+    
